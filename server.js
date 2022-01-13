e27d4a848b419466a7aa0592b9011bc50a7c0b00
tmantena1 <tmantena1@gmail.com>
2022-01-13 12:09:49 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require ('./routes');
+const logger = require("morgan");
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.use(express.static('public'))
+
+// Connect to mongoDB
+
+mongoose.connect(process.env.MONgODB_URI || "mongodb://localhost/workoutDB",{
+    useNewUrlParser: true,
+} );
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
