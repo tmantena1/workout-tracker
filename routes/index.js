e1be82394c77c19ff8443e65091bfebd9f3e3498
tmantena1 <tmantena1@gmail.com>
2022-01-13 12:35:31 -0500

    added html routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const htmlRoutes = require('./htmlRoutes');
 const apiRoutes = require('./apiRoutes');
 
+router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
